911b8a2dc2fa72595a6cca2907b479f81ec79971
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-10 15:16:01 +0100

    draft exercise

--- jsnack1/javascript/script.js
+++ jsnack1/javascript/script.js
@@ -1,2 +1,19 @@
 /* In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby,
 chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa. */
+
+// array guest list
+const guestList = ["Ciro", "Antonio", "Lucia", "Matteo", "Lorenzo", "Valeria"];
+
+const yourName = prompt("Inserisci il tuo nome", "Antonio");
+
+let feedback = "Non sei in lista";
+
+for (let i = 0; i < guestList.length; i++) {
+
+    if (yourName.trim() === guestList[i]) {
+        feedback = "Sei in lista"
+    }
+
+}
+
+console.log(feedback);
--- jsnack2/javascript/script.js
+++ jsnack2/javascript/script.js
@@ -1,2 +1,13 @@
 /* Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero,
 se è dispari inseriscilo nell’array. */
+
+// array number list
+const numberList = [];
+
+for (let i = 1; i <= 6; i++) {
+    const numberInput = prompt("Inserisci un numero");
+    if (!isNaN(numberInput) && numberInput % 2 !== 0) {
+        numberList.push(numberInput);
+    }
+}
+console.log(numberList);
