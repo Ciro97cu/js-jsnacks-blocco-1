60a34ce841d723a823ed2137d558cec6b11b177a
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-10 18:32:07 +0100

    completed exercise

--- jsnack3/javascript/script.js
+++ jsnack3/javascript/script.js
@@ -2,23 +2,25 @@
 tutte le cifre che compongono il numero. 
 (questo potrebbe richiedere un minimo di ricerca) */
 
-// const number = 1234;
-
-// const arrOfDigits = Array.from(String(number), Number);
-// console.log(arrOfDigits); // 👉️ [1, 2, 3, 4]
-
-
 document.querySelector("button").addEventListener("click",
     function () {
 
         let numberInput = document.querySelector("input").value;
-        const arrayOfDigits = Array.from(String(numberInput), Number);
-        console.log(arrayOfDigits);
-        let sumNumber;
-        for (let i = 0; i < arrayOfDigits.length; i++) {
-            sumNumber += parseInt(arrayOfDigits[i]);
+        let sumNumber = 0;
+        for (let i = 0; i < numberInput.length; i++) {
+            sumNumber += parseInt(numberInput[i]);
         }
         console.log(sumNumber);
+
+        // let numberInput = document.querySelector("input").value;
+        // const arrayOfDigits = Array.from(String(numberInput), Number);
+        // console.log(arrayOfDigits);
+        // let sumNumber = 0;
+        // for (let i = 0; i < arrayOfDigits.length; i++) {
+        //     sumNumber += arrayOfDigits[i];
+        // }
+        // console.log(sumNumber);
+        // document.querySelector("p").innerText = `La somma è ${sumNumber}`;
 
     }
 )
