7c3f7d710f1fd1e23c792809af0abd2640b05f94
Ciro Cusati <ciro.lavoro97@gmail.com>
2022-03-11 19:05:49 +0100

    added comments

--- jsnack1/javascript/script.js
+++ jsnack1/javascript/script.js
@@ -4,18 +4,23 @@ chiedi all’utente il suo nome e comunicagli se può partecipare o no alla fest
 // array guest list
 const guestList = ["Ciro", "Antonio", "Lucia", "Matteo", "Lorenzo", "Valeria"];
 
+// I ask the name of the user
 const yourName = prompt("Inserisci il tuo nome", "Antonio");
 
+// declare a boolean variable to stop the cycle when I find the name
 let exit = false;
 
 for (let i = 0; i < guestList.length && exit === false; i++) {
 
+    /* if the inserted name corresponds to one of the list,
+    the exit variable becomes true and the cycle stops */
     if (yourName.trim() === guestList[i]) {
         exit = true;
     }
 
 }
-
+/* I use a condition that prints a default message based 
+on the value of our boolean variable */
 if (exit) {
     console.log("Sei in lista");
 } else {
